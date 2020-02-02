bec218505ef58dbd23f03361c59b4223a827f255
zoommer1 <60567640+zoommer1@users.noreply.github.com>
2020-02-02 12:42:49 +0300

    Add files via upload

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+       public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Point p1 = new Point(1,3,'*');
+          
+            p1.Draw();
+
+            Point p2 = new Point(4,5,'#');
+            
+            p2.Draw();
+
+            Console.ReadLine();
+
+        }
+       
+    }
+}
